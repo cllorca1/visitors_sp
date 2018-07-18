10a8aa63df10988cbb0e25666fcd1cd6e4ad720f
cllorca1 <carlos.llorca@tum.de>
2018-07-18 17:13:48 +0200

    analyze results

---
+++ preparation/analyze_final_sp.R
@@ -0,0 +1,52 @@
+travelers_sp = travelers_sp %>% 
+  mutate(child = if_else(hp_alter < 18,1,0)) %>%
+  mutate(adult = if_else(hp_alter >= 18 & hp_alter < 65 ,1,0)) %>%
+  mutate(retired = if_else(hp_alter >= 65,1,0))
+
+party_composition = travelers_sp %>% group_by(unique_id) %>%
+  summarize(n_child = sum(child), n_adult=sum(adult), n_retired = sum(retired))
+
+parties_sp = merge(x=parties_sp, y = party_composition, by = "unique_id")
+
+
+cluster_data_parties = parties_sp %>% rowwise() %>% 
+  filter(main_mode != "denied", main_mode != "unaware", purpose != "denied", purpose !="unaware") %>%
+  filter(p1014 < 95, hheink < 15) %>%
+  mutate(is_car = if_else(main_mode == "car",1,0)) %>% 
+  mutate(is_train =  if_else(main_mode == "train",1,0)) %>% 
+  mutate(is_coach =  if_else(main_mode == "coach",1,0)) %>% 
+  mutate(is_airplane =  if_else(main_mode == "air",1,0)) %>% 
+  mutate(is_bicycle =  if_else(main_mode == "bicycle",1,0)) %>% 
+  mutate(is_ship = if_else(main_mode == "ship",1,0)) %>% 
+  mutate(is_leisure = if_else(purpose == "leisure",1,0)) %>% 
+  mutate(is_visit = if_else(purpose == "visit",1,0)) %>%
+  mutate(is_other_private = if_else(purpose == "other_private",1,0)) %>%
+  mutate(is_business = if_else(purpose == "business",1,0)) %>% 
+  mutate(is_commuter = if_else(purpose == "weekend_commuter",1,0)) %>% 
+  mutate(is_other = if_else(purpose == "other",1,0)) %>%
+  mutate(number_of_nights = p1014)
+  
+
+cluster_data_parties = cluster_data_parties %>% 
+  select(unique_id, number_of_nights, hheink,  
+         is_leisure, is_visit,  is_business, n_adult, n_child, n_retired)
+
+fit = kmeans(cluster_data_parties[2:ncol(cluster_data_parties)], 4)
+
+fit$centers
+
+plot_data = parties_sp %>% rowwise() %>% 
+  filter(main_mode != "denied", main_mode != "unaware", purpose != "denied", purpose !="unaware") %>%
+  filter(p1014 < 95, hheink < 15)
+
+plot_data$cluster = fit$cluster
+
+#analyze purpose by month
+ggplot(plot_data,
+       aes(x=as.factor(month), fill = as.factor(cluster))) +
+  geom_bar(position = "fill") +
+  xlab("Month") + ylab("Share of trips by cluster") +
+  theme_light() + 
+  theme(text=element_text(size=16, family="Times New Roman")) + 
+  labs(fill = "Purpose") + 
+  theme(legend.position = "bottom")
--- preparation/assign_variables.R
+++ preparation/assign_variables.R
@@ -13,19 +13,51 @@ parties_sp$unique_id = seq.int(nrow(parties_sp))
 
 write.csv(parties_sp, "C:/projects/visitors/result/trip_parties.csv", row.names = F)
 
+parties_sp = read.csv("C:/projects/visitors/result/trip_parties.csv")
+
 #assign persons in the household travelling
 
 travelers_sp = data.frame()
-for (id in parties$unique_id){
+missing_persons = 0
+count_persons = 0
+for (id in 1:nrow(parties_sp)){
   this_household_id = parties_sp$hhid[id]
   this_person_id = parties_sp$pid[id]
-  
+  #household of the traveler
   this_household = subset(persons, hhid  == this_household_id)
-  
-  this_person = subset(this_household, pid = this_household_id)
-  
-  
-  
+  #main traveler
+  this_person = subset(this_household, pid == this_person_id)
+  this_person$unique_id = id
+  this_person$month = parties_sp$month[id]
+  this_person$nights = parties_sp$nights[id]
+  this_person$purpose = parties_sp$purpose[id]
+  this_person$main_mode = parties_sp$main_mode[id]
+  count_persons = count_persons + 1
+  travelers_sp = rbind(travelers_sp, this_person)
+  this_household = subset(this_household, pid != this_person_id)
+  #rest of household members in the party
+  this_hh_party = parties_sp$hh_party[id] - 1
+  if (this_hh_party > 0 & this_hh_party <= nrow(this_household)){
+    count_persons = count_persons + this_hh_party 
+    if (this_hh_party < nrow(this_household)){
+      additional_person_ids = sample(this_household$pid, size = this_hh_party, replace = F)
+    } else if (this_hh_party == nrow(this_household)){
+      additional_persons_ids = this_household$pid
+    }
+    additional_persons = subset(this_household, pid %in% additional_persons_ids)
+    additional_persons$unique_id = id
+    additional_persons$month = parties_sp$month[id]
+    additional_persons$nights = parties_sp$nights[id]
+    additional_persons$purpose = parties_sp$purpose[id]
+    additional_persons$main_mode = parties_sp$main_mode[id]
+    travelers_sp = rbind(travelers_sp, additional_persons)
+  } else if (this_hh_party > nrow(this_household)){
+    #there are not sufficient persons in the household to fill this travel party (why?)
+    missing_persons = missing_persons + this_hh_party - nrow(this_household)
+  }
+  print(paste(id,count_persons,missing_persons, sep = "/"))
 }
+
+write.csv(travelers_sp, "C:/projects/visitors/result/persons.csv", row.names = F)
 
 
